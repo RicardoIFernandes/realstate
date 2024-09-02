11fa49fd19059cd0834a6a0d623f6af934109a41
RicardoIFernandes <rico191@gmail.com>
2024-09-02 17:37:03 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,151 +1,157 @@
-import streamlit as st
+import streamlit as st 
 import pandas as pd
-import math
-from pathlib import Path
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from bs4 import BeautifulSoup
+import time
 
-# Set the title and favicon that appear in the Browser's tab bar.
-st.set_page_config(
-    page_title='GDP dashboard',
-    page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
-)
+st.set_page_config(layout="wide")
 
-# -----------------------------------------------------------------------------
-# Declare some useful functions.
+st.sidebar.title('Montgomery County, MD')
+input_sites = st.sidebar.text_input('"geographic id list" here:')
+lista = input_sites.split(' ')
 
-@st.cache_data
-def get_gdp_data():
-    """Grab GDP data from a CSV file.
+def main():
 
-    This uses caching to avoid having to read the file every time. If we were
-    reading from an HTTP endpoint instead of a file, it's a good idea to set
-    a maximum age to the cache with the TTL argument: @st.cache_data(ttl='1d')
-    """
+    
+    st.write('You entered:', lista)
 
-    # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
-    DATA_FILENAME = Path(__file__).parent/'data/gdp_data.csv'
-    raw_gdp_df = pd.read_csv(DATA_FILENAME)
+    # Initialize empty lists
+    url_lista = []
+    owner_name_list = [] 
+    secondary_owner_name_list = []
+    property_address_list = []
+    owner_id_list = []
+    ownership_percentage_list = []
+    exemptions_list = []
+    account_number_list = [] 
+    state_code_list = []
 
-    MIN_YEAR = 1960
-    MAX_YEAR = 2022
+    property_id_list = []
+    geographic_id_list = []
+    tax_office_list = []
+    type_list = []
+    legal_list = [] 
+    property_use_list = []
 
-    # The data above has columns like:
-    # - Country Name
-    # - Country Code
-    # - [Stuff I don't care about]
-    # - GDP for 1960
-    # - GDP for 1961
-    # - GDP for 1962
-    # - ...
-    # - GDP for 2022
-    #
-    # ...but I want this instead:
-    # - Country Name
-    # - Country Code
-    # - Year
-    # - GDP
-    #
-    # So let's pivot all those year-columns into two: Year and GDP
-    gdp_df = raw_gdp_df.melt(
-        ['Country Code'],
-        [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
-        'Year',
-        'GDP',
-    )
+    land_homesite_list = []
+    land_non_homesite_list = []
+    special_use_list = []
+    total_land_list = []
+    improvement_list = []
+    improvement_non_live_list = []
+    total_improvement_list = []
+    market_value_list = []
+    special_use2_list = []
+    appraised_value_list = []
+    value_limitation_list = []
+    net_appraised_list = [] 
 
-    # Convert years from string to integers
-    gdp_df['Year'] = pd.to_numeric(gdp_df['Year'])
+    driver = webdriver.Chrome() 
 
-    return gdp_df
+    for i in lista:
+        url = 'https://mcad-tx.org/property-search'
+        driver.get(url)
+        time.sleep(2)
+        driver.find_element('xpath','//*[@id="searchInput"]').send_keys(i)
+        time.sleep(2)
+        driver.find_element('xpath','//*[@id="searchInput"]').send_keys(Keys.ENTER)
+        time.sleep(2)
+        driver.find_element(By.CLASS_NAME, 'sc-hEsumM').click()
+        time.sleep(5)
+        # driver.find_element('xpath','//*[@id="root"]/div/div/div/div[1]/div[3]/div/div[2]/div[3]/div[2]/div[1]/div/div[2]/div[2]/div[4]/div[1]/div[2]/div/div/div[3]/a').click()
 
-gdp_df = get_gdp_data()
+        html = driver.page_source
+        soup = BeautifulSoup(html, 'html.parser')
 
-# -----------------------------------------------------------------------------
-# Draw the actual page
+        # Extract data from soup using class names
+        url_lista.append(driver.current_url)
+        first_group = soup.find_all(class_="sc-eopZyb gcOyaR")
+        owner_name_list.append(first_group[0].text if first_group and first_group[0].text.strip() else 'Not found')
+        secondary_owner_name_list.append(first_group[1].text if len(first_group) > 1 and first_group[1].text.strip() else 'Not found')
+        property_address_list.append(first_group[2].text if len(first_group) > 2 and first_group[2].text.strip() else 'Not found')
+        owner_id_list.append(first_group[3].text if len(first_group) > 3 and first_group[3].text.strip() else 'Not found')
+        ownership_percentage_list.append(first_group[4].text if len(first_group) > 4 and first_group[4].text.strip() else 'Not found')
+        exemptions_list.append(first_group[5].text if len(first_group) > 5 and first_group[5].text.strip() else 'Not found')
+        account_number_list.append(first_group[6].text if len(first_group) > 6 and first_group[6].text.strip() else 'Not found')
+        state_code_list.append(first_group[7].text if len(first_group) > 7 and first_group[7].text.strip() else 'Not found')
 
-# Set the title that appears at the top of the page.
-'''
-# :earth_americas: GDP dashboard
+        second_group = soup.find_all(class_="sc-bYwvMP gBhcoQ")
+        property_id_list.append(second_group[0].text if second_group and second_group[0].text.strip() else 'Not found')
+        geographic_id_list.append(second_group[1].text if len(second_group) > 1 and second_group[1].text.strip() else 'Not found')
+        tax_office_list.append(second_group[2].text if len(second_group) > 2 and second_group[2].text.strip() else 'Not found')
+        type_list.append(second_group[3].text if len(second_group) > 3 and second_group[3].text.strip() else 'Not found')
+        legal_list.append(second_group[4].text if len(second_group) > 4 and second_group[4].text.strip() else 'Not found')
+        property_use_list.append(second_group[5].text if len(second_group) > 5 and second_group[5].text.strip() else 'Not found')
 
-Browse GDP data from the [World Bank Open Data](https://data.worldbank.org/) website. As you'll
-notice, the data only goes to 2022 right now, and datapoints for certain years are often missing.
-But it's otherwise a great (and did I mention _free_?) source of data.
-'''
-
-# Add some spacing
-''
-''
-
-min_value = gdp_df['Year'].min()
-max_value = gdp_df['Year'].max()
-
-from_year, to_year = st.slider(
-    'Which years are you interested in?',
-    min_value=min_value,
-    max_value=max_value,
-    value=[min_value, max_value])
-
-countries = gdp_df['Country Code'].unique()
-
-if not len(countries):
-    st.warning("Select at least one country")
-
-selected_countries = st.multiselect(
-    'Which countries would you like to view?',
-    countries,
-    ['DEU', 'FRA', 'GBR', 'BRA', 'MEX', 'JPN'])
-
-''
-''
-''
-
-# Filter the data
-filtered_gdp_df = gdp_df[
-    (gdp_df['Country Code'].isin(selected_countries))
-    & (gdp_df['Year'] <= to_year)
-    & (from_year <= gdp_df['Year'])
-]
-
-st.header('GDP over time', divider='gray')
-
-''
-
-st.line_chart(
-    filtered_gdp_df,
-    x='Year',
-    y='GDP',
-    color='Country Code',
-)
-
-''
-''
+        third_group = soup.find_all(class_="sc-kqlzXE friqHl")
+        land_homesite_list.append(third_group[0].text if third_group and third_group[0].text.strip() else 'Not found')
+        land_non_homesite_list.append(third_group[1].text if len(third_group) > 1 and third_group[1].text.strip() else 'Not found')
+        special_use_list.append(third_group[2].text if len(third_group) > 2 and third_group[2].text.strip() else 'Not found')
+        total_land_list.append(third_group[3].text if len(third_group) > 3 and third_group[3].text.strip() else 'Not found')
+        improvement_list.append(third_group[4].text if len(third_group) > 4 and third_group[4].text.strip() else 'Not found')
+        improvement_non_live_list.append(third_group[5].text if len(third_group) > 5 and third_group[5].text.strip() else 'Not found')
+        total_improvement_list.append(third_group[6].text if len(third_group) > 6 and third_group[6].text.strip() else 'Not found')
+        market_value_list.append(third_group[7].text if len(third_group) > 7 and third_group[7].text.strip() else 'Not found')
+        special_use2_list.append(third_group[8].text if len(third_group) > 8 and third_group[8].text.strip() else 'Not found')
+        appraised_value_list.append(third_group[9].text if len(third_group) > 9 and third_group[9].text.strip() else 'Not found')
+        value_limitation_list.append(third_group[10].text if len(third_group) > 10 and third_group[10].text.strip() else 'Not found')
+        net_appraised_list.append(third_group[11].text if len(third_group) > 11 and third_group[11].text.strip() else 'Not found')
 
 
-first_year = gdp_df[gdp_df['Year'] == from_year]
-last_year = gdp_df[gdp_df['Year'] == to_year]
 
-st.header(f'GDP in {to_year}', divider='gray')
 
-''
 
-cols = st.columns(4)
 
-for i, country in enumerate(selected_countries):
-    col = cols[i % len(cols)]
 
-    with col:
-        first_gdp = first_year[first_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-        last_gdp = last_year[last_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
 
-        if math.isnan(first_gdp):
-            growth = 'n/a'
-            delta_color = 'off'
-        else:
-            growth = f'{last_gdp / first_gdp:,.2f}x'
-            delta_color = 'normal'
+    driver.quit()
 
-        st.metric(
-            label=f'{country} GDP',
-            value=f'{last_gdp:,.0f}B',
-            delta=growth,
-            delta_color=delta_color
-        )
+    data_dict = {
+        # First group
+        'link': url_lista,
+        'owner_name_list': owner_name_list,
+        'secondary_owner_name_list': secondary_owner_name_list,
+        'property_address_list': property_address_list,
+        'owner_id_list': owner_id_list,
+        'ownership_percentage_list': ownership_percentage_list,
+        'exemptions_list': exemptions_list,
+        'account_number_list': account_number_list,
+        'state_code_list': state_code_list,
+
+        # Second group
+        'property_id_list': property_id_list,
+        'geographic_id_list': geographic_id_list,
+        'tax_office_list': tax_office_list,
+        'type_list': type_list,
+        'legal_list': legal_list,
+        'property_use_list': property_use_list,
+
+        # Third group
+        'land_homesite_list': land_homesite_list,
+        'land_non_homesite_list': land_non_homesite_list,
+        'special_use_list': special_use_list,
+        'total_land_list': total_land_list,
+        'improvement_list': improvement_list,
+        'improvement_non_live_list': improvement_non_live_list,
+        'total_improvement_list': total_improvement_list,
+        'market_value_list': market_value_list,
+        'special_use2_list': special_use2_list,
+        'appraised_value_list': appraised_value_list,
+        'value_limitation_list': value_limitation_list,
+        'net_appraised_list': net_appraised_list
+    }
+
+
+
+
+    df = pd.DataFrame(data_dict)
+    st.write(df)
+
+if len(lista) >= 2:
+    st.write(len(lista))
+    main()
+
